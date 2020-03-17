002923cfa3db390124cb6eb6ab4d45baee0d6843
stijn <stijn@ignitron.net>
2020-03-17 14:07:37 +0100

    datetime: Add tzinfo.__new__
    This is required just to make importing datetime even work, but it's not
    clear whether it's sufficient to get a fully working module: test_datetime
    doesn't pass all tests, not with CPython and not with MicroPython.

--- datetime/datetime.py
+++ datetime/datetime.py
@@ -914,6 +914,10 @@ class tzinfo:
     Subclasses must override the name(), utcoffset() and dst() methods.
     """
     __slots__ = ()
+
+    def __new__(cls, *args, **kwargs):
+        return object.__new__(cls)
+
     def tzname(self, dt):
         "datetime -> string name of time zone."
         raise NotImplementedError("tzinfo subclass must override tzname()")
